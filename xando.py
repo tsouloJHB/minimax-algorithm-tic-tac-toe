a599ac289ece255f35fbb488212ea86e3356f6a5
tsoulo <tsoulo@student.wethinkcode.co.za>
2021-06-19 00:09:18 +0200

    update

--- xando.py
+++ xando.py
@@ -1,8 +1,8 @@
 import random
-player1 = ""
-player2 = ""
-grid = ""
-board = []
+player1 = "x"
+player2 = "o"
+grid = []
+player = ""
 def check_positions(x,y):
    global grid
    #check xaxis
@@ -49,6 +49,136 @@ def check_positions(x,y):
             grid[2][2] == symbol:
             return False    
         symbol = "o"
+   return True
+
+
+def checkdraw(board):
+    global grid
+    #board = grid
+    if check_positions(2,3):
+        for x in range(0,3):
+            for y in range(0,3):
+                if board[x][y] == "":
+                    return False
+        return True
+    return False                
+
+
+def bestmoves():
+    global grid
+    global player2
+    grid = create_grid()
+    board = grid.copy()
+    board = [["x","o","o"],["o","x","x"],["x","o","o"]]
+    bestVal = -1000
+    bestMove = (-1,-1)
+    for t in board:
+        print(t)
+    
+    #iterate the grid
+    for a,x in enumerate(board):
+        for b,y in enumerate(x):
+            #check if cell is empty
+            if board[a][b] == "":
+                board[a][b] = player1
+                #moveVal = minimax(board,0,False)
+                moveVal = 0  
+                board[a][b] = ""
+                if moveVal > bestVal:
+                    bestMove = (a,b)
+                    moveVal = bestVal
+    print("The best move is " ,bestMove)
+
+    return bestMove                
+    
+def who_won(board):
+    global grid
+    #check xaxis
+    symbol = "x"
+    for d in range(0,2):
+
+        
+        if grid[2][2] == symbol and grid[2][1] == symbol and\
+            grid[2][0] == symbol:
+            return symbol
+        
+           
+        if grid[1][1] == symbol and grid[1][0] == symbol and\
+            grid[1][2] == symbol:
+            return symbol
+        
+        if grid[0][0] == symbol and grid[0][1] == symbol and\
+            grid[0][2] == symbol:
+            return symbol
+        
+        #check yaxis
+        
+        if grid[2][2] == symbol and grid[1][2] == symbol and\
+            grid[0][2] == symbol:
+            return symbol
+        
+        if grid[1][1] == symbol and grid[0][1] == symbol and\
+            grid[2][1] == symbol:
+            return symbol
+        
+        if grid[0][0] == symbol and grid[1][0] == symbol and\
+            grid[2][0] == symbol:
+            return symbol
+        #chech diagonal
+       
+    
+        if grid[0][2] == symbol and grid[1][1] == symbol and\
+            grid[2][0] == symbol:
+            return symbol
+        if grid[0][0] == symbol and grid[1][1] == symbol and\
+            grid[2][2] == symbol:
+            return symbol    
+        symbol = "o"
+    return ""
+
+
+def minimax(board,depth,maximize_player):
+    global player1
+    global player2 
+    #Check for winner
+    #computer wins
+    if check_positions(2,2) == False and player1 == who_won(board):
+        return 100
+    #player wins    
+    if check_positions(2,2) == False and player2 == who_won(board):
+        return -100
+
+    #check for draw and return 0  
+    if checkdraw(board):
+        return 0
+    #if maxizer wins
+    
+    if(maximize_player):
+        bestscore = -1000
+        #look for empty cells
+        for i in range(0,3):
+            for x in range(0,3):
+                if board[i][x] == "":
+                    board[i][x] = player1
+                    moveVal = minimax(board,0,False)  
+                    board[i][x] = ""
+                    if moveVal > bestscore:
+                        bestscore = moveVal
+        return bestscore
+    else:
+        bestscore = 800
+        #look for empty cells
+        for i in range(0,3):
+            for x in range(0,3):
+                if board[i][x] == "":
+                    board[i][x] = player2
+                    moveVal = minimax(board,0,True)  
+                    board[i][x] = ""
+                    if moveVal < bestscore:
+                        bestscore = moveVal
+        return bestscore
+    return 1
+
 def set_postion():
     pass
 
@@ -108,12 +238,20 @@ def play_game():
             continue  
         update_grid(move,turn)
         draw_grid()
+        print(who_won([],"player1")) 
+        #minimax([],2,False)  
         if check_positions(a,b) == False:
            break         
         if turn == "player1":
            turn = "player2"
         else:
            turn = "player1"
+   
+        
+        """   
+        if(checkdraw()):
+            print("its a draw") 
+        """      
     print(turn," won",)
     print("") 
  
@@ -200,4 +338,8 @@ def start_game():
     draw_grid() 
     play_game()
 
-gameloop()
+#board = [["x","","o"],["o","","x"],["x","","x"]]
+#grid = board
+#minimax(board,4,True)
+#gameloop()
+bestmoves()
